3d254511f992a7cb017451d27ef85520e807599e
Carlos Alve <carlosalves369m@hotmail.com>
2022-05-21 20:06:40 -0300

    :card_file_box: create database

---
+++ src/database/people.ts
@@ -0,0 +1,40 @@
+const PeopleData = [
+    {
+      uid: 'a421e4ef-74ad-40eb-8ebd-f511887bb6c0',
+      name: 'Carlos Alves',
+      year_of_birth: 1998,
+      image: 'images/carlos_alves.jpg',
+    },
+    {
+      uid: 'e71d0225-f6a5-4a92-a173-d29420d29e61',
+      name: 'Marcos Antonio',
+      year_of_birth: 2001,
+      image: 'images/marcos_antonio.jpg',
+    },
+    {
+      uid: '657d1253-250b-42ed-9705-a63509e45d90',
+      name: 'Pamela Rodriguez',
+      year_of_birth: 1998,
+      image: 'images/pamela_rodriguez.jpg',
+    },
+    {
+      uid: '601127a1-a33c-4ac2-95a7-f1e42b205f6a',
+      name: 'Fernanda Melo',
+      year_of_birth: 1997,
+      image: 'images/fernanda_melo.jpg',
+    },
+    {
+      uid: '2bc9b68c-6f68-431b-b65f-16c99aee0f26',
+      name: 'Claudio Gregório',
+      year_of_birth: 1999,
+      image: 'images/claudio_gregorio.jpg',
+    },
+    {
+      uid: '5459ebda-6cbe-45bc-a282-5c78691342d2',
+      name: "Maria Lima",
+      year_of_birth: 2003,
+      image: 'images/maria_lima.jpg'
+    }
+];
+
+export { PeopleData }
